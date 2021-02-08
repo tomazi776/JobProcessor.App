3749945e3fba824b1ef4f51e1b97f655293247e9
Tomasz_Urbaniak <tomasz.urbaniak@zaven.pl>
2021-02-08 16:10:07 +0100

    Add EF with Initial migration and JobProcessorContext

---
+++ JobProcessor.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace JobProcessor.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<JobProcessor.DataAccess.JobProcessorContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(JobProcessor.DataAccess.JobProcessorContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
