47a8b0b11efa04d79bee757e61b2f721ef013563
Tomasz_Urbaniak <tomasz.urbaniak@zaven.pl>
2021-02-09 16:52:06 +0100

    Add server side validation, with migration

--- JobProcessor.DataAccess/Entities/Job.cs
+++ JobProcessor.DataAccess/Entities/Job.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JobProcessor.DataAccess.Entities
 {
     public class Job
     {
         public Guid Id { get; set; }
+        [StringLength(50)]
+        [Index(IsUnique = true)]
         public string Name { get; set; }
         public int Counter { get; set; }
         public int StatusFlag { get; set; }
