9b66fc774b081ee63c7a3a1157c12aa8fcfcfece
Tomasz_Urbaniak <tomasz.urbaniak@zaven.pl>
2021-02-09 14:17:43 +0100

    Add Jobs Controller with respective ViewModel

---
+++ JobProcessor.App/Controllers/JobsController.cs
@@ -0,0 +1,18 @@
+﻿using JobProcessor.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobProcessor.App.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Jobs
+        public ActionResult Create(string name, DateTime? doAfter)
+        {
+            return View();
+        }
+    }
+}
---
+++ JobProcessor.App/ViewModels/JobCreationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobProcessor.App.ViewModels
+{
+    public class JobCreationViewModel
+    {
+        [Display(Name = "Unique Job name")]
+        [Required(ErrorMessage = "Enter unique name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Do after (Optional)")]
+        public DateTime? DoAfter { get; set; }
+    }
+}
