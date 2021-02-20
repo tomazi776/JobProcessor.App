e905e9675bcfff4d72167d18182bb96b2046c75c
Tomasz_Urbaniak <tomasz.urbaniak@zaven.pl>
2021-02-20 21:45:04 +0100

    Add ProcessedAt column to Jobs table

---
+++ JobProcessor.DataAccess/Migrations/202102202034284_Add_ProcessedAt_Date_To_Jobs_Table.cs
@@ -0,0 +1,18 @@
+﻿namespace JobProcessor.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ProcessedAt_Date_To_Jobs_Table : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Jobs", "ProcessedAt", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Jobs", "ProcessedAt");
+        }
+    }
+}
--- JobProcessor.Domain/Services/IntermediaryMapper.cs
+++ JobProcessor.Domain/Services/IntermediaryMapper.cs
@@ -4,9 +4,9 @@ namespace JobProcessor.Domain.Services
     public class IntermediaryMapper : IIntermediaryMappingService
     {
         public Models.Job MapDALToDomainModel(DataAccess.Entities.Job job) => 
-            new Models.Job(job.Id, job.Name, job.StatusFlag, job.DoAfter, job.CreatedAt, job.UpdatedAt, job.Counter);
+            new Models.Job(job.Id, job.Name, job.StatusFlag, job.DoAfter, job.CreatedAt, job.UpdatedAt, job.ProcessedAt, job.Counter);
         
         public DataAccess.Entities.Job MapDomainToDALModel(Models.Job job) => 
-            new DataAccess.Entities.Job(job.Id, job.Name, job.Status, job.DoAfter, job.CreatedAt, job.UpdatedAt, job.Counter);
+            new DataAccess.Entities.Job(job.Id, job.Name, job.Status, job.DoAfter, job.CreatedAt, job.UpdatedAt, job.ProcessedAt, job.Counter);
     }
 }
